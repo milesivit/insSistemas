fd3c6e66c1206c5b70fe3611f4eb357e0641dc28
milena <m.sivit@itecriocuarto.org.ar>
2025-04-24 18:01:54 -0300

    new thingss

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from home.views import register_view
+from home.views import (
+    home_view,
+    login_view,
+    logout_view,
+    register_view,
+    )
 
 urlpatterns = [
-    path('register/', register_view, name='register')
-    
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('', home_view, name='index'),
 ]
 
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def register_view(request):
@@ -16,20 +17,49 @@ def register_view(request):
             messages.error(request, "no data")
         
         elif pass1 != pass2:
-            messages.error(request, "the password dont match")
+            messages.error(request, "Passwords dont match")
         
         elif User.objects.filter(username=username).exists():
             messages.error(request, "the user is in use")
         
         else:
-            User.objects.create_user(
+            user = User.objects.create_user(
                 username=username,
                 password=pass1,
                 email=email
             )
+            login(request, user)  #loguea automáticamente
+            messages.success(request, "Registration successful")
+            return redirect('index')  #redirige a la home
         
 
     return render(
         request=request,
         template_name='accounts/register.html'
         )
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request, 
+            username=username,
+            password=password
+        )
+        if user is not None: #verifica si el usuario esta bien logueado
+            login(request, user)
+            messages.success(request, 'sesion success')
+            return redirect('index')
+        else:
+            messages.error(request, 'user or password invalid')
+        
+    return render(request, "accounts/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def home_view(request):
+    return render(request, 'index.html')
